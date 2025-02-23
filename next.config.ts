4bf228fd8a9e4f81288e5758fba3331e616b688e
Emir Segovia <95300946+Apache90@users.noreply.github.com>
2025-02-22 23:24:18 -0300

    config para deploy 2

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import {withSentryConfig} from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint:{
-    ignoreDuringBuilds: true,
-  }
-};
+const nextConfig: NextConfig = {};
 
 export default withSentryConfig(nextConfig, {
 // For all available options, see:
